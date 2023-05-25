4dd26e77112733494cc29a86ebc5f2c1e4ca2d34
joe95-ux <ogorktabi@gmail.com>
2023-05-25 23:50:24 +0100

    setup login and register controllers

---
+++ controllers/auth.js
@@ -0,0 +1,11 @@
+const registerUser = (req, res)=>{
+    res.send('register from callback');
+}
+
+const loginUser = (req, res)=>{
+    res.send('login from callback');
+}
+
+
+module.exports = {registerUser, loginUser};
+
--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const express = require("express");
 const connectDB = require("./config/db");
 const app = express();
 const mongoose = require("mongoose");
+const {registerUser, loginUser} = require('./controllers/auth');
 
 // connect to db
 connectDB();
-app.get('/', (req, res)=>{
-    res.send("Hello World");
-})
+app.get('/api/register', registerUser);
+app.get('/api/login', loginUser);
+
+
 
 
 const port = process.env.PORT || 8080;
